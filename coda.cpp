74726431d0e5ec5aa445cd92a294ca8c5924318f
alessandromazzitelli <mazzitelli.alessandro2205@gmail.com>
2024-12-06 10:38:12 +0100

    coda.cpp

---
+++ coda.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+template <typename T>
+class Coda{
+  private:
+    ListaPuntatori<T> lista;
+  public:
+    void enqueue(T elem){
+      lista.inslista(elem, lista.lunghezza() + 1);
+    }
+
+    T dequeue(){
+        if(lista.listavuota(){
+          return lista.lung==0;
+        }else{
+          T valore = lista.leggilista(1); 
+          lista.canclista(1);
+          return valore;
+    }
+
+    T front() const{
+        if(lista.listavuota(){
+          return lista.lung==0;
+        }else{
+          return lista.leggilista(1);
+        }
+    }
+
+    bool isEmpty() const{
+      return lista.listavuota();
+    }
+
+    int size() const{
+      return lista.lunghezza();
+    }
+   }
+
+    
+
+    
+    
